ec886a52a926c4167254bc077e6445173b32d34a
jakesumneryale <103459280+jakesumneryale@users.noreply.github.com>
2024-08-15 12:51:50 -0400

    Update create_protein_graph_structure.py
    small changes

--- create_protein_graph_structure.py
+++ create_protein_graph_structure.py
@@ -23,227 +23,255 @@ parser.add_argument("--output_file", "-o", help="The name of the output file to 
 ####### GRAPH GENERATION FUNCTIONS #######
 ##########################################
 
-def get_protein_coords(pdb_name, pdb_dir):
-    '''
-    Gets the coordinates of the c-alpha and 
-    heavy atoms  - in separate lists for simplicity -
-    for each amino acids in the protein entered.
-    
-    Each chain will be separated into it's own list as well. 
-    '''
-    
-    os.chdir(pdb_dir)
-    
-    ## Init the pdb parser
-    
-    pdb_parser = Bio.PDB.PDBParser(QUIET = True)
 
-    ## Get the target structure 
-    
-    heterodimer = pdb_parser.get_structure(pdb_name.split(".")[0], pdb_name)
-    het_model = heterodimer[0]
-    
-    dimer_ca = []
-    dimer_ha = []
-    aa_list = []
-    
-    first_chain_bool = True
-    
-    for chain in het_model:
-        temp_chain_list_ca = []
-        temp_chain_list_ha = []
-        count = 0
-        for residue in chain:
-            temp_res_list = []
-            count +=1
-            aa_list.append(residue.get_name())
-            for atom in residue:
-                if "CA" in atom.get_name():
-                    temp_chain_list_ca.append(atom.get_coord())
-                    temp_res_list.append(atom.get_coord())
-                    
-                elif "H" not in atom.get_name()[0] and atom.get_name()[0] not in ["1", "2", "3", "4"]:
-                    temp_res_list.append(atom.get_coord())
-                    
-            temp_chain_list_ha.append(np.array(temp_res_list))
-        
-        print("Chain count:", count)
-        dimer_ca.append(np.array(temp_chain_list_ca))
-        dimer_ha.append(temp_chain_list_ha)
-    
-    return dimer_ca, dimer_ha, aa_list
-        
+aa_three_to_one = {
+	'ALA': 'A',  # Alanine
+	'ARG': 'R',  # Arginine
+	'ASN': 'N',  # Asparagine
+	'ASP': 'D',  # Aspartic acid
+	'CYS': 'C',  # Cysteine
+	'GLU': 'E',  # Glutamic acid
+	'GLN': 'Q',  # Glutamine
+	'GLY': 'G',  # Glycine
+	'HIS': 'H',  # Histidine
+	'ILE': 'I',  # Isoleucine
+	'LEU': 'L',  # Leucine
+	'LYS': 'K',  # Lysine
+	'MET': 'M',  # Methionine
+	'PHE': 'F',  # Phenylalanine
+	'PRO': 'P',  # Proline
+	'SER': 'S',  # Serine
+	'THR': 'T',  # Threonine
+	'TRP': 'W',  # Tryptophan
+	'TYR': 'Y',  # Tyrosine
+	'VAL': 'V',  # Valine
+}
+
+def get_protein_coords(pdb_name, pdb_dir):
+	'''
+	Gets the coordinates of the c-alpha and 
+	heavy atoms  - in separate lists for simplicity -
+	for each amino acids in the protein entered.
+	
+	Each chain will be separated into it's own list as well. 
+	'''
+
+	## Load in global dictionary
+
+	global aa_three_to_one
+	
+	os.chdir(pdb_dir)
+	
+	## Init the pdb parser
+	
+	pdb_parser = Bio.PDB.PDBParser(QUIET = True)
+
+	## Get the target structure 
+	
+	heterodimer = pdb_parser.get_structure(pdb_name.split(".")[0], pdb_name)
+	het_model = heterodimer[0]
+	
+	dimer_ca = []
+	dimer_ha = []
+	aa_list = []
+	
+	first_chain_bool = True
+	
+	for chain in het_model:
+		temp_chain_list_ca = []
+		temp_chain_list_ha = []
+		count = 0
+		for residue in chain:
+			temp_res_list = []
+			count +=1
+			aa_list.append(aa_three_to_one[residue.get_resname()])
+			for atom in residue:
+				if "CA" in atom.get_name():
+					temp_chain_list_ca.append(atom.get_coord())
+					temp_res_list.append(atom.get_coord())
+					
+				elif "H" not in atom.get_name()[0] and atom.get_name()[0] not in ["1", "2", "3", "4"]:
+					temp_res_list.append(atom.get_coord())
+					
+			temp_chain_list_ha.append(np.array(temp_res_list))
+		
+		print("Chain count:", count)
+		dimer_ca.append(np.array(temp_chain_list_ca))
+		dimer_ha.append(temp_chain_list_ha)
+	
+	return dimer_ca, dimer_ha, aa_list
+		
 ## Create the adjacency matrix from the heavy atom coordinates
 
 def get_distances_between_heavy_atom_lists(atoms1, atoms2, cutoff_distance=4.5):
-    '''
-    Returns 1 if the cutoff distance between the two
-    amino acids in atoms1 and atoms2 is below 4.5 for any
-    of the atoms in the array
-    '''
-    
-    aa_dist_mat = distance_matrix(atoms1, atoms2)
-        
-    norm_mat = np.where(aa_dist_mat <= cutoff_distance, 1, 0)
-    
-    return np.max(norm_mat)
+	'''
+	Returns 1 if the cutoff distance between the two
+	amino acids in atoms1 and atoms2 is below 4.5 for any
+	of the atoms in the array
+	'''
+	
+	aa_dist_mat = distance_matrix(atoms1, atoms2)
+		
+	norm_mat = np.where(aa_dist_mat <= cutoff_distance, 1, 0)
+	
+	return np.max(norm_mat)
 
 def create_adjacency_matrix(dimer_ha, cutoff_distance=4.5):
-    '''
-    Creates the adjacency matrix from the heavy atoms of each dimer.
-    First the intra-chain distances are calculated and then the 
-    inter-chain distances.
-    
-    The edges are created between residues that are within the cutoff_distance
-    from one another, which is set to 4.5Å by default. Covalent bonds also count as edges.
-    '''
-    
-    ## Define the chains
-    
-    chain1 = dimer_ha[0]
-    chain2 = dimer_ha[1]
-    
-    chain1_len = len(chain1)
-    chain2_len = len(chain2)
-    tot_len = chain1_len + chain2_len
-    
-    ## Create the adjacency matrix structure
-    
-    final_adj_mat = np.zeros((tot_len, tot_len))
-    
-    ## Loop through each amino acid and add it to the adjacency matrix
-    
-    ## Loop through chain 1 first and do intra chain distances
-    
-    for i in range(chain1_len):
-        res_i = chain1[i]
-        for j in range(i,chain1_len):
-            res_j = chain1[j]
-            
-            edge_val = get_distances_between_heavy_atom_lists(res_i, res_j, cutoff_distance = cutoff_distance)
-            
-            final_adj_mat[i,j] = edge_val
-            final_adj_mat[j,i] = edge_val
-        
-    ## Loop through chain 2 now and do intra chain distances
-    
-    for i in range(chain2_len):
-        res_i = chain2[i]
-        for j in range(i,chain2_len):
-            res_j = chain2[j]
-            
-            new_i = i + chain1_len
-            new_j = j + chain1_len
-            
-            edge_val = get_distances_between_heavy_atom_lists(res_i, res_j, cutoff_distance = cutoff_distance)
-            
-            final_adj_mat[new_i,new_j] = edge_val
-            final_adj_mat[new_j,new_i] = edge_val
-        
-    
-    ## Now do the inter chain distances
-    
-    for i in range(chain1_len):
-        res_i = chain1[i]
-        for j in range(chain2_len):
-            res_j = chain2[j]
-            
-            new_ind = j + chain1_len
-            
-            edge_val = get_distances_between_heavy_atom_lists(res_i, res_j, cutoff_distance = cutoff_distance)
-            
-            final_adj_mat[i,new_ind] = edge_val
-            final_adj_mat[new_ind, i] = edge_val
-            
-    return final_adj_mat
-        
-    
+	'''
+	Creates the adjacency matrix from the heavy atoms of each dimer.
+	First the intra-chain distances are calculated and then the 
+	inter-chain distances.
+	
+	The edges are created between residues that are within the cutoff_distance
+	from one another, which is set to 4.5Å by default. Covalent bonds also count as edges.
+	'''
+	
+	## Define the chains
+	
+	chain1 = dimer_ha[0]
+	chain2 = dimer_ha[1]
+	
+	chain1_len = len(chain1)
+	chain2_len = len(chain2)
+	tot_len = chain1_len + chain2_len
+	
+	## Create the adjacency matrix structure
+	
+	final_adj_mat = np.zeros((tot_len, tot_len))
+	
+	## Loop through each amino acid and add it to the adjacency matrix
+	
+	## Loop through chain 1 first and do intra chain distances
+	
+	for i in range(chain1_len):
+		res_i = chain1[i]
+		for j in range(i,chain1_len):
+			res_j = chain1[j]
+			
+			edge_val = get_distances_between_heavy_atom_lists(res_i, res_j, cutoff_distance = cutoff_distance)
+			
+			final_adj_mat[i,j] = edge_val
+			final_adj_mat[j,i] = edge_val
+		
+	## Loop through chain 2 now and do intra chain distances
+	
+	for i in range(chain2_len):
+		res_i = chain2[i]
+		for j in range(i,chain2_len):
+			res_j = chain2[j]
+			
+			new_i = i + chain1_len
+			new_j = j + chain1_len
+			
+			edge_val = get_distances_between_heavy_atom_lists(res_i, res_j, cutoff_distance = cutoff_distance)
+			
+			final_adj_mat[new_i,new_j] = edge_val
+			final_adj_mat[new_j,new_i] = edge_val
+		
+	
+	## Now do the inter chain distances
+	
+	for i in range(chain1_len):
+		res_i = chain1[i]
+		for j in range(chain2_len):
+			res_j = chain2[j]
+			
+			new_ind = j + chain1_len
+			
+			edge_val = get_distances_between_heavy_atom_lists(res_i, res_j, cutoff_distance = cutoff_distance)
+			
+			final_adj_mat[i,new_ind] = edge_val
+			final_adj_mat[new_ind, i] = edge_val
+			
+	return final_adj_mat
+		
+	
 
 
 def create_pdb_from_ca_coords(dimer_ca, adj_mat, output_file_start, edge_type='all'):
-    '''
-    Creates a pdb file that has single spheres
-    over the C-alpha coordinates for each amino acid.
-    It also connects the amino acids according to the CONECT record
-    '''
-    
-    ## Make the edge type text lowercase
-    
-    edge_type = edge_type.lower()
-    
-    edge_type_accept_list = ["all", "interface", "intrachain"]
-    
-    if edge_type not in edge_type_accept_list:
-        print("ERROR: PLEASE ENTER IN AN ACCEPTABLE EDGE TYPE: ALL, INTERFACE, INTRACHAIN")
-        return 1
-    
-    output_file = f"{output_file_start}_{edge_type}.pdb"
-    
-    ## Begin the loop to write out the nodes
-    
-    total_i = 0
-    
-    with open(output_file, 'w') as pdb_file:
-        
-        ## Write the coordinates for the C-alpha files - which are the node locations!
-        pdb_file.write("HEADER    GENERATED GRAPH FOR GNN PROJECT\n")
-        for cnum, chain in enumerate(dimer_ca):
-            for i, (x, y, z) in enumerate(chain, start=1):
-                residue_number = i + total_i
-                atom_number = i + total_i
-                if cnum == 0:
-                    chain_name = "A"
-                elif cnum == 1:
-                    chain_name = "B"
-                pdb_file.write(
-                    f"ATOM  {atom_number:5d}  CA  ALA {chain_name}{residue_number:4d}    "
-                    f"{x:8.3f}{y:8.3f}{z:8.3f}  1.00  0.00           C\n"
-                )
-            pdb_file.write("TER\n")
-            total_i += i
-            
-        ## Specify edges using the adjacency matrix and CONECT
-        
-        chain1_len = len(dimer_ca[0])
-        chain2_len = len(dimer_ca[1])
-        total_len = chain1_len + chain2_len
-        
-        ## The full graph
-        
-        if edge_type == "all":
-        
-            for i in range(total_len):
-                for j in range(i, total_len):
-                    if adj_mat[i,j] == 1 and i != j:
-                        pdb_file.write(f"CONECT{i+1:5d}{j+1:5d}\n")
+	'''
+	Creates a pdb file that has single spheres
+	over the C-alpha coordinates for each amino acid.
+	It also connects the amino acids according to the CONECT record
+	'''
+	
+	## Make the edge type text lowercase
+	
+	edge_type = edge_type.lower()
+	
+	edge_type_accept_list = ["all", "interface", "intrachain"]
+	
+	if edge_type not in edge_type_accept_list:
+		print("ERROR: PLEASE ENTER IN AN ACCEPTABLE EDGE TYPE: ALL, INTERFACE, INTRACHAIN")
+		return 1
+	
+	output_file = f"{output_file_start}_{edge_type}.pdb"
+	
+	## Begin the loop to write out the nodes
+	
+	total_i = 0
+	
+	with open(output_file, 'w') as pdb_file:
+		
+		## Write the coordinates for the C-alpha files - which are the node locations!
+		pdb_file.write("HEADER    GENERATED GRAPH FOR GNN PROJECT\n")
+		for cnum, chain in enumerate(dimer_ca):
+			for i, (x, y, z) in enumerate(chain, start=1):
+				residue_number = i + total_i
+				atom_number = i + total_i
+				if cnum == 0:
+					chain_name = "A"
+				elif cnum == 1:
+					chain_name = "B"
+				pdb_file.write(
+					f"ATOM  {atom_number:5d}  CA  ALA {chain_name}{residue_number:4d}    "
+					f"{x:8.3f}{y:8.3f}{z:8.3f}  1.00  0.00           C\n"
+				)
+			pdb_file.write("TER\n")
+			total_i += i
+			
+		## Specify edges using the adjacency matrix and CONECT
+		
+		chain1_len = len(dimer_ca[0])
+		chain2_len = len(dimer_ca[1])
+		total_len = chain1_len + chain2_len
+		
+		## The full graph
+		
+		if edge_type == "all":
+		
+			for i in range(total_len):
+				for j in range(i, total_len):
+					if adj_mat[i,j] == 1 and i != j:
+						pdb_file.write(f"CONECT{i+1:5d}{j+1:5d}\n")
 
-        ## The interface edges only
-        
-        elif edge_type == "interface":
-                    
-            for i in range(chain1_len):
-                for j in range(chain1_len, total_len):
-                    if adj_mat[i,j] == 1 and i != j:
-                        pdb_file.write(f"CONECT{i+1:5d}{j+1:5d}\n")
-                        
-        ## Intrachain edges only (no interface edges)
-                        
-        elif edge_type == "intrachain":
-            
-            ## Chain 1
-            for i in range(chain1_len):
-                for j in range(i,chain1_len):
-                    if adj_mat[i,j] == 1 and i != j:
-                        pdb_file.write(f"CONECT{i+1:5d}{j+1:5d}\n")
-                      
-            ## Chain 2
-            for i in range(chain1_len,total_len):
-                for j in range(i,total_len):
-                    if adj_mat[i,j] == 1 and i != j:
-                        pdb_file.write(f"CONECT{i+1:5d}{j+1:5d}\n")
-            
-        
-    return 0
+		## The interface edges only
+		
+		elif edge_type == "interface":
+					
+			for i in range(chain1_len):
+				for j in range(chain1_len, total_len):
+					if adj_mat[i,j] == 1 and i != j:
+						pdb_file.write(f"CONECT{i+1:5d}{j+1:5d}\n")
+						
+		## Intrachain edges only (no interface edges)
+						
+		elif edge_type == "intrachain":
+			
+			## Chain 1
+			for i in range(chain1_len):
+				for j in range(i,chain1_len):
+					if adj_mat[i,j] == 1 and i != j:
+						pdb_file.write(f"CONECT{i+1:5d}{j+1:5d}\n")
+					  
+			## Chain 2
+			for i in range(chain1_len,total_len):
+				for j in range(i,total_len):
+					if adj_mat[i,j] == 1 and i != j:
+						pdb_file.write(f"CONECT{i+1:5d}{j+1:5d}\n")
+			
+		
+	return 0
 
 #################################################
 ####### NODE FEATURE GENERATION FUNCTIONS #######
@@ -268,7 +296,7 @@ def feature_interface_one_hot(adj_mat, node_ind, chain1_len):
 		## Part of chain 1
 		interface_slice = adj_mat[node_ind, chain1_len:]
 
-	elif node_ind <= chain1_len:
+	elif node_ind >= chain1_len:
 		## Part of chain 2
 		interface_slice = adj_mat[node_ind, :chain1_len]
 
@@ -286,7 +314,7 @@ def feature_interface_node_degree(adj_mat, node_ind, chain1_len):
 		## Part of chain 1
 		interface_slice = adj_mat[node_ind, chain1_len:]
 
-	elif node_ind <= chain1_len:
+	elif node_ind >= chain1_len:
 		## Part of chain 2
 		interface_slice = adj_mat[node_ind, :chain1_len]
 
@@ -305,7 +333,7 @@ def feature_intrachain_node_degree(adj_mat, node_ind, chain1_len):
 		## Part of chain 1
 		intrachain_slice = adj_mat[node_ind, :chain1_len]
 
-	elif node_ind <= chain1_len:
+	elif node_ind >= chain1_len:
 		## Part of chain 2
 		intrachain_slice = adj_mat[node_ind, chain1_len:]
 
@@ -337,26 +365,26 @@ def feature_chain_id(node_ind, chain1_len, total_len):
 #### Global amino acid reference list
 
 amino_acid_reference_list = [
-    'A',  # Alanine
-    'R',  # Arginine
-    'N',  # Asparagine
-    'D',  # Aspartic acid
-    'C',  # Cysteine
-    'E',  # Glutamic acid
-    'Q',  # Glutamine
-    'G',  # Glycine
-    'H',  # Histidine
-    'I',  # Isoleucine
-    'L',  # Leucine
-    'K',  # Lysine
-    'M',  # Methionine
-    'F',  # Phenylalanine
-    'P',  # Proline
-    'S',  # Serine
-    'T',  # Threonine
-    'W',  # Tryptophan
-    'Y',  # Tyrosine
-    'V',  # Valine
+	'A',  # Alanine
+	'R',  # Arginine
+	'N',  # Asparagine
+	'D',  # Aspartic acid
+	'C',  # Cysteine
+	'E',  # Glutamic acid
+	'Q',  # Glutamine
+	'G',  # Glycine
+	'H',  # Histidine
+	'I',  # Isoleucine
+	'L',  # Leucine
+	'K',  # Lysine
+	'M',  # Methionine
+	'F',  # Phenylalanine
+	'P',  # Proline
+	'S',  # Serine
+	'T',  # Threonine
+	'W',  # Tryptophan
+	'Y',  # Tyrosine
+	'V',  # Valine
 ]
 
 
@@ -383,26 +411,26 @@ def feature_one_hot_amino_acid_type(node_ind, aa_list):
 
 
 amino_acid_charges = {
-    'A': 0.0,   # Alanine
-    'R': 1.0,   # Arginine
-    'N': 0.0,   # Asparagine
-    'D': -1.0,  # Aspartic acid
-    'C': 0.0,   # Cysteine
-    'E': -1.0,  # Glutamic acid
-    'Q': 0.0,   # Glutamine
-    'G': 0.0,   # Glycine
-    'H': 0.1,   # Histidine (slight positive charge at pH=7)
-    'I': 0.0,   # Isoleucine
-    'L': 0.0,   # Leucine
-    'K': 1.0,   # Lysine
-    'M': 0.0,   # Methionine
-    'F': 0.0,   # Phenylalanine
-    'P': 0.0,   # Proline
-    'S': 0.0,   # Serine
-    'T': 0.0,   # Threonine
-    'W': 0.0,   # Tryptophan
-    'Y': 0.0,   # Tyrosine
-    'V': 0.0,   # Valine
+	'A': 0.0,   # Alanine
+	'R': 1.0,   # Arginine
+	'N': 0.0,   # Asparagine
+	'D': -1.0,  # Aspartic acid
+	'C': 0.0,   # Cysteine
+	'E': -1.0,  # Glutamic acid
+	'Q': 0.0,   # Glutamine
+	'G': 0.0,   # Glycine
+	'H': 0.1,   # Histidine (slight positive charge at pH=7)
+	'I': 0.0,   # Isoleucine
+	'L': 0.0,   # Leucine
+	'K': 1.0,   # Lysine
+	'M': 0.0,   # Methionine
+	'F': 0.0,   # Phenylalanine
+	'P': 0.0,   # Proline
+	'S': 0.0,   # Serine
+	'T': 0.0,   # Threonine
+	'W': 0.0,   # Tryptophan
+	'Y': 0.0,   # Tyrosine
+	'V': 0.0,   # Valine
 }
 
 
@@ -461,7 +489,7 @@ def feature_amino_acid_hydrophobicity(node_ind, aa_list):
 	return hydrophobicity_amino_acids[node_aa_id]
 
 
-    
+	
 def generate_node_features(adj_mat, aa_list, chain1_len, chain2_len):
 	'''
 	Generates the following node features
@@ -557,7 +585,7 @@ def main():
 	## Create the node features for the graph
 
 	chain1_len = len(dimer_ca[0])
-    chain2_len = len(dimer_ca[1])
+	chain2_len = len(dimer_ca[1])
 
 	graph_node_features = generate_node_features(dimer_adj_mat, aa_list, chain1_len, chain2_len)
 
